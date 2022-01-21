64a6ff2bada757d0cb4c80130d83047271076d99
ayang <ay764920@ohio.edu>
2022-01-21 15:44:04 -0500

    commit my git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Andrew Yang
+ *        Date: 1/21/2022
+ * Description: 2nd lab for CS 2400
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
